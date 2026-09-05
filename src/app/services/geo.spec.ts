3fcb7e75ac5d64ee95a38906b294e4c7d79602e4
jsoul-dev <oblakxz27@gmail.com>
2026-09-05 17:05:50 +0800

    generate service: geo

---
+++ src/app/services/geo.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { Geo } from './geo';
+
+describe('Geo', () => {
+  let service: Geo;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(Geo);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
