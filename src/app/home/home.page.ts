5c714e2ca3da3183ac61593636b0ab811a880d15
jsoul-dev <oblakxz27@gmail.com>
2026-09-05 17:08:01 +0800

    implement start/stop location watching with watchPosition and clearWatch

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+import { AfterViewInit, Component, effect, inject } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonFooter,
+  IonAlert
+} from '@ionic/angular';
+import * as L from 'leaflet';
+import { Geo } from '../services/geo';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFooter, IonAlert],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements AfterViewInit {
+  private geoService = inject(Geo);
+  private map!: L.Map;
+
+  latitude: number = 0;
+  longitude: number = 0;
+  myLivePosition: any = null;
+  isWatching: boolean = false;
+
+  isMyAlertOpen: boolean = false;
+  myHeader: string = '';
+  myMessage: string = '';
+  alertButtons = ['OK'];
+
+  constructor() {
+    effect(() => {
+      const liveCoordinates = this.geoService.myLiveLotLong();
+      if (liveCoordinates) {
+        this.myLivePosition = liveCoordinates;
+        this.latitude = liveCoordinates.lats;
+        this.longitude = liveCoordinates.lungs;
+      }
+    });
+  }
+
+  async ngAfterViewInit(): Promise<void> {
+    const start = await this.geoService.getLatLng();
+    if (start) {
+      this.latitude = start.latitude;
+      this.longitude = start.longitude;
+      this.initMap();
+    } else {
+      this.myHeader = 'Geolocation API Error';
+      this.myMessage = 'Unable to get your current location. Please check your GPS and permissions.';
+      this.isMyAlertOpen = true;
+    }
+  }
+
+  initMap(): void {
+    this.map = L.map('mapa').setView([this.latitude, this.longitude], 19);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    setInterval(() => {
+      this.map.invalidateSize();
+    }, 200);
+  }
+
+  async onStartWatching(): Promise<void> {
+    await this.geoService.startWatching();
+    this.isWatching = true;
+  }
+
+  async onStopWatching(): Promise<void> {
+    await this.geoService.stopWatching();
+    this.isWatching = false;
+  }
 }
--- src/app/services/geo.ts
+++ src/app/services/geo.ts
@@ -1,5 +1,46 @@
-import { Service } from '@angular/core';
+import { Service, signal } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Service()
 export class Geo {
+  myLiveLotLong = signal<any>(null);
+  private watchId: string | undefined;
+
+  async getLatLng() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      return {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      };
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+
+  async startWatching() {
+    this.watchId = await Geolocation.watchPosition(
+      { enableHighAccuracy: true },
+      (position, err) => {
+        if (position) {
+          this.myLiveLotLong.set({
+            lats: position.coords.latitude,
+            lungs: position.coords.longitude
+          });
+        }
+      }
+    );
+  }
+
+  async stopWatching() {
+    try {
+      if (this.watchId) {
+        await Geolocation.clearWatch({ id: this.watchId });
+        this.watchId = undefined;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
